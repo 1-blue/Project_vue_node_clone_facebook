a27bb15f2963090ba226804d9c2fe937eeede57f
1-blue <ghksaud55@naver.com>
2021-06-24 12:49:05 +0900

    server) Implement post fetch logic

--- clone-facebook-server/routes/post.js
+++ clone-facebook-server/routes/post.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const router = require("express").Router();
 const { isLoggedIn } = require("../middlewares/auth.js");
-const { Post } = require("../models/index.js");
+const { User, Post } = require("../models/index.js");
 
 // 포스트 업로드
 router.post("/", isLoggedIn, async (req, res) => {
@@ -12,6 +12,25 @@ router.post("/", isLoggedIn, async (req, res) => {
       title,
       contents,
       UserId: req.user._id,
+    });
+
+    return res.json({ response });
+  } catch (error) {
+    return res.status(500).json({ message: "서버측 에러입니다. 잠시후에 다시 시도해주세요.", error });
+  }
+});
+
+// 포스트 가져오기
+router.get("/", isLoggedIn, async (req, res) => {
+  // const { pageNumber } = req.body;
+  // 이거 스크롤 내릴때마다 호출하고 5 ~ 10개씩 추가적으로 호출되도록 수정필요
+
+  try {
+    const response = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ["name"],
+      },
     });
 
     return res.json({ response });
